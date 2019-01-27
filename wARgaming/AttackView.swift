4ba04d7a0497b1a08d9402a17c696c6f96b187eb
Sebastian Nikles <sebastian.nikles@tum.de>
2019-01-27 23:56:51 +0100

    Implements the attack rolls

--- wARgaming/AttackView.swift
+++ wARgaming/AttackView.swift
@@ -30,6 +30,35 @@ class AttackView: UIView, NibLoadable {
     var attackingCharacter: Character!
     var defendingCharacter: Character!
     
+    var completionAction: (() -> ())!
+    
+    var phase: AttackPhase! {
+        didSet {
+            switch phase {
+            case .HitRoll?:
+                rollButton.setTitle("Hit Roll", for: .normal)
+            case .WoundRoll?:
+                rollButton.setTitle("Wound Roll", for: .normal)
+            case .SavingThrow?:
+                rollButton.setTitle("Saving Throw", for: .normal)
+            case .Success?:
+                rollButton.setTitle("End", for: .normal)
+                attackStateLabel.text = "The attack was successful"
+            default:
+                rollButton.setTitle("Close", for: .normal)
+                attackStateLabel.text = "The attack has failed"
+            }
+        }
+    }
+    
+    enum AttackPhase {
+        case HitRoll
+        case WoundRoll
+        case SavingThrow
+        case Ended
+        case Success
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setupFromNib()
@@ -40,9 +69,12 @@ class AttackView: UIView, NibLoadable {
         setupFromNib()
     }
     
-    func setUp(_ attacker: Character, _ defender: Character) {
+    func setUp(_ attacker: Character, _ defender: Character, completion: @escaping (() -> ())) {
+        phase = .HitRoll
+        
         attackingCharacter = attacker
         defendingCharacter = defender
+        completionAction = completion
 
         ballisticSkillLabel.text = "\(attacker.ballisticSkill)+"
         rolledBallisticSkillLabel.text = ""
@@ -61,5 +93,71 @@ class AttackView: UIView, NibLoadable {
     }
     
     @IBAction func roll(_ sender: Any) {
+        if phase == .HitRoll {
+            handleHitRoll()
+        } else if phase == .WoundRoll {
+            handleWoundRoll()
+        } else if phase == .SavingThrow {
+            handleSavingThrow()
+        } else {
+            completionAction()
+        }
+    }
+    
+    func handleHitRoll() {
+        let roll = arc4random_uniform(6) + 1
+        
+        rolledBallisticSkillLabel.text = String(roll)
+        
+        defendingCharacter.wounds -= attackingCharacter.weaponDamage
+        phase = .Success
+        return
+        
+        if roll == 1 || roll < attackingCharacter.ballisticSkill {
+            phase = .Ended
+            rolledBallisticSkillLabel.textColor = UIColor.red
+        } else {
+            phase = .WoundRoll
+        }
+    }
+    
+    func handleWoundRoll() {
+        let roll = arc4random_uniform(6) + 1
+        let requiredRoll: Int!
+        
+        if attackingCharacter.weaponStrength >= 2 * defendingCharacter.toughness {
+            requiredRoll = 2
+        } else if attackingCharacter.weaponStrength >= defendingCharacter.toughness {
+            requiredRoll = 3
+        } else if attackingCharacter.weaponStrength == defendingCharacter.toughness {
+            requiredRoll = 4
+        } else if 2 * attackingCharacter.weaponStrength < defendingCharacter.toughness {
+            requiredRoll = 6
+        } else {
+            requiredRoll = 5
+        }
+        
+        rolledWSLabel.text = String(roll)
+        
+        if roll == 1 || roll < requiredRoll {
+            phase = .Ended
+            rolledWSLabel.textColor = UIColor.red
+        } else {
+            phase = .SavingThrow
+        }
+    }
+    
+    func handleSavingThrow() {
+        let roll = arc4random_uniform(6) + 1
+        
+        rolledSVLabel.text = String(roll)
+        
+        if roll > 1 && Int(roll) - attackingCharacter.armorPenetration >= defendingCharacter.save {
+            phase = .Ended
+        } else {
+            phase = .Success
+            rolledSVLabel.textColor = UIColor.red
+            defendingCharacter.wounds -= attackingCharacter.weaponDamage
+        }
     }
 }
